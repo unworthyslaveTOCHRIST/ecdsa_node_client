b5b0204e419be5f4e45b10200b53f64b5ac747d8
unworthyslaveTOCHRIST <righteousnessbyfaithinjesus@gmail.com>
2025-12-17 23:27:37 +0100

    graciously completed first alchemy project

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const server = axios.create({
-  // baseURL: "https://ecdsa-node-server-neon.vercel.app",
-  baseURL: "http://localhost:3042",
+  baseURL: "https://ecdsa-node-server-neon.vercel.app",
+  // baseURL: "http://localhost:3042",
 });
 
 export default server;
